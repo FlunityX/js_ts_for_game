dd87fe01bd051c3032ad3c7f37667794d87bb688
nguyễn mạnh <manharia123@gmail.com>
2024-02-07 12:57:46 +0700

    hàm , kiểu dữ liệu , in và ép kiểu js

---
+++ main.js
@@ -0,0 +1,17 @@
+let x = 5;
+let y = 8;
+let z = x + y;
+console.log(z);
+
+let s = String(x);
+console.log(s);
+
+let bool = Boolean(a);
+console.log(bool);
+
+function tongxyz(x, y, z) {
+  return x + y + z;
+}
+
+let b = tongxyz(x, y, z);
+console.log(b);
