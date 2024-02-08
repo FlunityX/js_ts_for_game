51ede37da4e61776f880e2067e1f6b7318c8480b
nguyễn mạnh <manharia123@gmail.com>
2024-02-08 13:00:39 +0700

    tạo đối tượng , mảng và duyệt phần tử

---
+++ script.js
@@ -0,0 +1,17 @@
+// Tạo đối tượng person
+const person = { name: "Lili", age: 18, city: "New York" };
+console.log(person);
+
+// Mảng 1 chiều
+const a = [1, 2, 3, 4, 5];
+
+// Hiển thị giá trị chỉ mục
+console.log(a[1]);
+
+// Hiển thị độ dài của mảng
+console.log(a.length);
+
+// Duyệt mảng sử dụng vòng lặp for
+for (let i = 0; i < a.length; i++) {
+  console.log("Phần tử thứ " + i + " là " + a[i]);
+}
