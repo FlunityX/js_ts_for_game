012252e4fd37aa675cbfa3e7e229cee76762f54a
nguyễn mạnh <manharia123@gmail.com>
2024-02-08 13:02:37 +0700

    in ra màn hình khai báo và enum

---
+++ script.ts
@@ -0,0 +1,18 @@
+const message: string = "Hello, TypeScript!";
+console.log(message);
+function add(a: number, b: number) {
+  return a + b;
+}
+const result = add(3,5)
+console.log(result);
+enum onweek{
+  monday ,
+  tueday,
+  wesnezdays,
+  thursday,
+  friday,
+  sartuday,
+  sunday
+}
+let current = onweek.sartuday
+console.log(current.toString);
