3e6df4d7b2e721c8d0459c2cd62e9822d4a870f8
nguyễn mạnh <manharia123@gmail.com>
2024-02-13 19:32:41 +0700

    toán tư js

---
+++ mainjs.js
@@ -0,0 +1,15 @@
+// == để chỉ "=" về giá trị
+console.log("4"==4)
+// === để chỉ "=" về kiểu dữ liệu và giá trị
+console.log("4"===4)
+console.log(100000011 << 1)
+// ToExponenttial (x) --> x số thập phân 
+let a= 12.4314652463547654
+console.log(a.toExponential(2))
+// parse Int,float
+let b ="55.634a535243"
+console.log(parseFloat(b))
+console.log(parseInt(b))
+//bigint
+console.log(BigInt("0xF"));// mã hexa
+console.log(typeof(5n));
